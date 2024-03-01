884fa8625d1cc4ff5c4828a859e762a01febb183
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-01 20:28:52 +0530

    Update Intro_Js.js

--- Intro_Js.js
+++ Intro_Js.js
@@ -1,1 +1,19 @@
-What Is Javascript
+What Is Javascript?
+    It is an scripting Language.
+    We can create content dynamically using javascript.
+    95% websies are built using js.
+    brendon ech is known as father of js.
+    mocha --> livescript --> javascript.
+    Js is an Interpreted language.
+    Js is an object based scripting lang.
+    in it polymorphison encapsulation features are there.
+    Js is easy to learn.
+    It can be use both in frontend and backend.
+    There are many frameworks in Js:
+        Angular
+        React
+        Node, etc
+    
+    
+    
+    
