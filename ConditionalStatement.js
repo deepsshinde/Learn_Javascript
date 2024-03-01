ece5342a523016d4badb38cd8a16ecb830406de1
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-01 22:43:51 +0530

    Create ConditionalStatement.js

---
+++ ConditionalStatement.js
@@ -0,0 +1,32 @@
+CONDITONAL STATEMENT:
+  There are 3 types of conditional statements.
+
+  1. IF STATEMENT:
+      let age = 20;
+      if(age >18){
+        console.log("You can vote");
+      }
+
+  2. IF-ELSE STATEMENT:
+    let mode = "dark";
+      if(mode == "light")
+        {
+            color = "white";
+        }
+        else{
+            color = "black";
+        }
+
+  3. ELSE-IF STATEMENT:
+      let mode = "pink";
+      if(mode == "light")
+        {
+            color = "white";
+        }
+        else if(mode = "pink"){
+            color = "pink";
+        }
+        else
+        {
+            color = "black";
+        } 
