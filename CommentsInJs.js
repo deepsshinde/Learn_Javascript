a068c5e84df91e80609ec9dabc1b4fd6bd251990
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-01 21:56:01 +0530

    Create CommentsInJs.js

---
+++ CommentsInJs.js
@@ -0,0 +1,11 @@
+Comments are the part of code which is not executed.
+
+  for single line comments we can use //
+   // this is a single line comment
+
+for multiple line commens we can use /* */
+/* 
+  this is 
+  multiple
+  line comment
+  */
