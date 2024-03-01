2d5c808ac0015b510d9460fecc6f9e868af7b987
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-01 21:51:47 +0530

    Create Practice.js

---
+++ Practice.js
@@ -0,0 +1,22 @@
+const product = {
+Fname : "Ball Pen",
+rating: 4,
+offer : 5,
+price : 90,
+};
+
+
+const profile = {
+  Name : "ShraddhaKhapra",
+  Posts : 195,
+  Follower : 569K,
+  following : 4,
+  Threads : "@shraddhakhapra",
+  Bio : "Apna College | Ex-Microsoft, DRDO To Educate someone is the highest privilege",
+};
+
+// to get type of this 
+typeof profile -- it will give object
+typeof profile["Name"]; -- it will give string.
+and like that we can get typeof indivisual element from an object.
+
