eb80e38b63a3e43911d37fa8d3408cbe987544cc
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-01 21:41:49 +0530

    Create DataTypes.js

---
+++ DataTypes.js
@@ -0,0 +1,31 @@
+Data Types are of two types
+
+1. Primitive Data Type :
+     there are 7 data types
+       i. Numeric Data Type - It can be positive or can be negative.
+      ii. String - 
+     iii. Boolean - it has two values. true and false
+      iv. Undefined - it has no value
+       v. NULL - Absence of an object
+      vi. BigInt - It doesnt Used widely
+     vii. Symbol - It doesnt Used widely
+
+2. Non primitives:
+    i. Objects: - It is a collection of values. It stores data in Key:Value Pairs.(Deeptii : 20)
+      generally we declare objects useing const keyword.
+      
+const student = {
+    Fname: "Deeptii",
+    age : 20,
+    marks : 9.4,
+    isPass: true,
+};
+
+We can access using object many ways:\
+    typeof (student) - it will give "Object"
+    student - It will print content of student in key:value pair as {Fname:Deeptii, age:20, CGPA:9.4}
+    student["age"];
+     
+
+        i. Arrays
+       ii. Functions
