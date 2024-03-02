a6b2e01b352898ecbf63dc67e353358a9dc51bc9
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-02 14:16:49 +0530

    Create StringsInJS.js

---
+++ StringsInJS.js
@@ -0,0 +1,14 @@
+STRING IN JAVASCRIPT:
+  String is a sequence of characters used to represent text.
+  whenever we want to write the text or name we use stirng.
+  we can write string using " " or ' ' 
+
+  let str = "deeptii";
+  let str = 'deeptii'; 
+
+  String has inbuilt properties and functions.
+
+    PROPERTIES :
+      1. Length Property :   It is used to give length of a string
+          syntax  -   str.length
+    
