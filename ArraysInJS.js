81a4acde41bff705ff22468cfa942757ca73c34b
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-06 13:39:07 +0530

    Create ArraysInJS.js

---
+++ ArraysInJS.js
@@ -0,0 +1,51 @@
+ARRAYS:
+        Arrays are nothis but the collections of items.
+        To store same type of informatin we use arrays.
+        it store items in linear manner.
+        Arrays are internaly object.
+        Arrays in Js are Mutable. 
+        means we can make changes in arrray once declared.
+        
+        Syntax:
+               let marks = [1, 2, 3, 4]
+               console.log(marks)
+                typeof marks  // it will print as an object
+
+        using for loops:
+        Syntax.
+                let marks = [90,79,68,99]
+                for (let i = 0; i<marks.length; i++)
+                        {
+                                 marks
+                        }
+
+        using while loop.
+        Syntax.
+                let marks =  [90,79,68,99]
+                let i=0
+                while(i <= marks.length){
+                    console.log(marks[i])
+                        i++
+                }
+
+
+        using do-while loop.
+        syntax.
+                let marks =  [90,79,68,99]
+                let i=0
+                do{
+                console.log(marks[i])
+                i++
+                }
+                while(i<=marks.length)
+
+                
+                
+
+
+
+
+
+
+                
+  
