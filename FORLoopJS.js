717a783de538b31be1cb0afa46adf344da29a959
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-06 11:23:41 +0530

    Update and rename LoopsInJS.js to FORLoopJS.js

---
+++ FORLoopJS.js
@@ -0,0 +1,18 @@
+LOOPS IN JAVASCRIPT:
+    Lops are used to execute a piece of code again and again.
+    There are many types of loops available.
+    1. for loop :
+          i.     //printin 
+                  for(let i=1; //initialization condition
+                    i<=5; //Termination COndition
+                    i++  //Increment/decrement 
+                   ){
+                        console.log("Deeptii");
+                }
+         ii.     //calculate sum of 1 to 5 
+                 let sum = 0;
+                 for(let n=1; n<=5; n++){
+                 sum +=n;
+                 }
+                console.log("SUm is:", sum);
+  
