9e71bf5f920c06ea77284f7f5b9f025e82bec48d
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-06 11:30:08 +0530

    Create DO-Whileloop.js

---
+++ DO-Whileloop.js
@@ -0,0 +1,12 @@
+    3. do while loop:
+            Syntax:
+                    do{
+                            //do some work
+                    }while(i<=5);
+
+                let i=8;
+                do{
+                        console.log("Hello");
+                        i++;
+                }while(i<=10);
+ 
