6a538cd65f6d2153a70d4f2b2884ac21fd5239b5
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-06 11:29:30 +0530

    Create WHILELoop.js

---
+++ WHILELoop.js
@@ -0,0 +1,12 @@
+               
+    2. while loop:
+            syntax:
+                    while(condition){
+                            //code goes here
+                    }
+                let i=1;
+                while(i<=5){
+                        console.log(i);
+                        i++;
+                }
+
