d5798349d18eae031df77f6f9a50bd2024b27728
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-08 20:07:41 +0530

    Create Pattern1.js

---
+++ Pattern1.js
@@ -0,0 +1,19 @@
+//Printing pattern
+/*
+
+****
+***
+**
+*
+
+*/
+let n=3
+for(let i=0; i<n; i++)
+  {
+    for(j=0; j<n-i; j++)
+      {
+        process.stdout.write("*")
+
+      }
+    console.log()
+  } 
