1661aa201d020240a4d35ef1e5ffb3d74456f4db
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-08 14:23:11 +0530

    Create ArrayMethods.js

---
+++ ArrayMethods.js
@@ -0,0 +1,20 @@
+Array Methods :
+        there are various type of mwthods in an array.
+
+        1. Push().
+                It it ised to add an element at the end of an array.
+                it maks changes in original array.
+                synatx:
+                        var.push("The item which we want insert")
+                        fooditems.push("Potato", "apple")
+                        cosole.log(fooditems)
+        2. pop().
+                It is used to delete an value from an array.
+                it deletes a values from end of an array.
+                synatx .
+                        var.push("some values").
+                example:
+                        fooditem.pop("potato")
+
+        3. toString().
+                it returns a new string whic converts an array to string.
