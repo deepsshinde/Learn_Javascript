3cdcc316b2b6d8561c6bb80da8fea9a2219d01bd
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-08 21:06:25 +0530

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,8 @@
+FUNCTIONS:
+      It is a block of a code that performs specific task, that can be onvoked whenevr needed.
+      Syntax.:
+              functions def                         
+                function funname(){ //do some work}
+                        
+             function call.
+                        functionname();
