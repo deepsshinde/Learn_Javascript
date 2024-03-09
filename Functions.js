733f3186cb759009fa443b2fe692db313c139dd2
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-10 05:29:10 +0530

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,8 +1,13 @@
 FUNCTIONS:
-      It is a block of a code that performs specific task, that can be onvoked whenevr needed.
+      It is a block of a code that performs specific task, that can be onvoked(call) whenevr needed.
       Syntax.:
               functions def                         
                 function funname(){ //do some work}
                         
              function call.
                         functionname();
+
+
+
+
+ 
