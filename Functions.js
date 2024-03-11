f756cd9a3c859adbb1186edb4b67fb98ee7ebf33
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-11 09:23:38 +0530

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,13 +1,11 @@
 FUNCTIONS:
       It is a block of a code that performs specific task, that can be onvoked(call) whenevr needed.
-      Syntax.:
-              functions def                         
-                function funname(){ //do some work}
-                        
-             function call.
-                        functionname();
+      Functions can be built in or user defined.
+      we can pass some parameters in it.
+      local vaiable is nothing but the variables which are acessible by that function only.
+      functions parameters are like local variable of function.
+      it has block scope.
+      we can store functions in variable.
 
 
-
-
- 
+          
