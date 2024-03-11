334b2eaa8e8a3f1c96bc2b4cc5b149fb0039d809
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-11 09:24:14 +0530

    Create ArrowFunctions..js

---
+++ ArrowFunctions..js
@@ -0,0 +1,43 @@
+  Arrow Functions :
+            generally we write multiple funcitons like this.
+                  //sum function
+            function sum(a, b){
+                  return a + b
+            }
+            function mu(a, b){
+                  return a * b
+            }
+
+            but using arrow functions we can make them smaller as we wanted.
+            => this is a sign of a arrow functions.
+            Arrow functons is the type of Mordern Javascript.
+
+                  const arrowMul = ( a ,b ) =>{
+                        return a * b
+                  }
+                        arrowMul( 4, 5)
+            
+      
+      Syntax.:
+              functions def                         
+                function funname(){ 
+                      //do some work
+                }
+                        
+             function call.
+                        functionname();
+      example :.
+            //functions
+            function myFunctions(){
+              console.log("Hello World")
+            }
+            myFunctions()
+
+
+
+
+
+
+
+
+ 
