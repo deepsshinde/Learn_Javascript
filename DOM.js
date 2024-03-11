ffaa721843032e0949fada7a87a86f7f962f80d4
Dipti Deepak Shinde <120267784+deepsshinde@users.noreply.github.com>
2024-03-11 22:19:54 +0530

    Update and rename DOM.txt to DOM.js

--- DOM.js
+++ DOM.js
@@ -1,6 +1,8 @@
 WINDOW OBJECT:.
         the window object represents an open window in a browser. it browers object( not js's) & automatically created byt the browser
         it is aglobal object with lots of prooperties & methods.
+        to make changes in html without actually changing in html we can use DOM.
+        
         
 
 
@@ -20,13 +22,31 @@ DOM: .
         meta meta title link             div                script
                                   img h1 p duv
 
-DOM MANIPULATION:.
+DOM MANIPULATION SELECTORS:.
         DOM Maniulation is nothing but the accessing the elements.
         we can access element using various ways.
 
         1. ID:
                 to define id in css we use #.
                 document.getElementById("some value")
-        2. 
+        2. Class:
+                to define the class in css file we use "."
+                document.getElementByClassName("MYclass")
 
-        
+        3. Tag:
+                document.getElementByTagName("p")
+
+        4. Query Selector:.
+                in query selector we can pass id, class, or tagname to access the element.
+                document.queryselector("p")
+
+
+DOM MANIPULATION PROPERTIRS:.
+        1. tagName :
+                It returns tag for element nodes.
+        2. innerText :
+                It returns text context of the element and all its childre.
+        3. innerHTML :
+                It returns the plain text or HTML contents in the element.
+        4. textContent :
+                It returns textual content even for hidden elements.
